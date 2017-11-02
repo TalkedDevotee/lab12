9e69473c828b9d6da5f1c1f6b52dbf9a030f009f
TalkedDevotee <jokerpokerface@yandex.ru>
2017-11-02 18:36:58 +0300

    first commit

---
+++ sources/demo.cpp
@@ -0,0 +1,12 @@
+#include <print.hpp>
+
+int main(int argc, char** argv)
+{
+std::string text;
+while(std::cin >> text)
+{
+std::ofstream out("log.txt", std::ios_base::app);
+print(text, out);
+out << std::endl;
+}
+}
