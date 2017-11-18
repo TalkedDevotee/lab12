6e3566aa3cf7e89c5ad4080952035298e7e4a0d4
TalkedDevotee <jokerpokerface@yandex.ru>
2017-11-18 23:40:18 +0300

    CURL

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# The Travis Client [![Build Status](https://travis-ci.org/TalkedDevotee/lab10.svg?branch=master)](https://travis-ci.org/TalkedDevotee/lab10)
+# The Travis Client [![Build Status](https://travis-ci.org/TalkedDevotee/lab12.svg?branch=master)](https://travis-ci.org/TalkedDevotee/lab12)
 ![The Travis Mascot](http://about.travis-ci.org/images/travis-mascot-200px.png)
-the demo application redirects data from stdin to a file **log.txt** using a package **print**.
---
+++ cget.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <curl/curl.h>
+
+using namespace std;
+
+void GetResponse(char* &url)
+{
+   CURL *curl;
+   CURLcode res;
+   curl = curl_easy_init();
+   if (curl)
+   {
+      curl_easy_setopt(curl, CURLOPT_URL, url);
+      curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+      res = curl_easy_perform(curl);
+      if (res == CURLE_OK)
+      {
+         long response_code = 0;
+         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+         cout << "Response answer: " << response_code << endl;
+      }
+      else
+      {
+         cout << "Code 'curl_easy_perform()' failed: " << curl_easy_strerror(res) << endl;
+      }
+      curl_easy_cleanup(curl);
+   }
+}
+
+int main(int argc, char* argv[])
+{
+   char *url = argv[1];
+   GetResponse(url);
+   return 0;
+}
